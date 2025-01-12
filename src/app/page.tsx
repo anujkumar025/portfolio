8597ff6f870648a481e032e0a6abcf8b27ca6317
anujkumar025 <ffriction73@gmail.com>
2025-01-12 23:08:52 +0530

    initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,104 @@
-import Image from "next/image";
+import Image from 'next/image';
+import profile from '../../public/profile.jpeg';
+import typescript from '../../public/typescript.svg';
+import react from '../../public/react.svg';
+import css from '../../public/css.png';
+import express from '../../public/express.svg';
+import mongodb from '../../public/mongodb.svg';
+import nestjs from '../../public/nestjs.svg';
+import postgresql from '../../public/postgresql.svg';
+import tailwind from '../../public/tailwind-css.svg';
+import nextjs from '../../public/next.svg';
+import prisma from '../../public/prisma.svg';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import procure_sense from './../components/assets/procure_sense.png';
+import typeSprint from './../components/assets/type-sprint.png';
+import cardGame from './../components/assets/card-game.png';
+import voteApp from './../components/assets/voting-app.png';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className= "w-screen flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center w-2/5 my-10 space-y-4">
+          <div className='flex items-center space-x-4 w-full'>
+            <div><Image src={profile} width={80} height={80} alt="image" className='rounded-full aspect-square object-cover'/></div>
+            <div>
+              <div className='text-xl font-bold'>Hey, I'm Anuj Kumar</div>
+              <div className='text-sm'>Full Stack Developer</div>
+            </div>
+          </div>
+          <div>
+            <div>I'm a creative software developer with five years of experience. I specialize in UI design and crafting engaging user experiences with great attention to detail.</div>
+          </div>
+          <div className='grid grid-cols-5 gap-7'>
+            <div className='flex justify-center items-center'><Image src={typescript} alt='TypeScript' width={40} height={40}/></div>
+            <div className='flex justify-center items-center'><Image src={react} alt='Reactjs' width={40} height={40}/></div>
+            <div className='flex justify-center items-center'><Image src={css} alt='css' width={40} height={40}/></div>
+            <div className='flex justify-center items-center'><Image src={express} alt='Express' width={40} height={40}/></div>
+            <div className='flex justify-center items-center'><Image src={mongodb} alt='MongoDB' width={40} height={40}/></div>
+            <div className='flex justify-center items-center'><Image src={nestjs} alt='NestJs' width={40} height={40}/></div>
+            <div className='flex justify-center items-center'><Image src={postgresql} alt='Postgresql' width={40} height={40}/></div>
+            <div className='flex justify-center items-center'><Image src={tailwind} alt='Tailwind' width={40} height={40}/></div>
+            <div className='flex justify-center items-center'><Image src={nextjs} alt='NestJs' width={40} height={40}/></div>
+            <div className='flex justify-center items-center'><Image src={prisma} alt='Prisma' width={40} height={40}/></div>
+          </div>
+          <div className='flex justify-start w-full text-lg'>
+            <div>Projects</div>
+          </div>
+          <div className='grid grid-cols-2 gap-4'>
+            <Card className='hover:bg-hoverWhite'>
+              <a href="https://tender-six.vercel.app/" target="_blank" rel="noopener noreferrer">
+                <CardHeader>
+                  <CardTitle>Procure Sense</CardTitle>
+                  <CardDescription>Check eligibility of proposals</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <Image src={procure_sense} alt="project-1" height={150}/>
+                </CardContent>
+              </a>
+            </Card>
+            <Card className='hover:bg-hoverWhite'>
+              <a href="https://type-sprint-navy.vercel.app/" target="_blank" rel="noopener noreferrer">
+                <CardHeader>
+                  <CardTitle>Type Sprint</CardTitle>
+                  <CardDescription>Know your typing speed and improve</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <Image src={typeSprint} alt="project-1" height={150}/>
+                </CardContent>
+              </a>
+            </Card>
+            <Card className='hover:bg-hoverWhite'>
+              <a href="https://66e5bd4df79a9a948515cbb2--verdant-squirrel-faca2f.netlify.app/" target="_blank" rel="noopener noreferrer">
+                <CardHeader>
+                  <CardTitle>Procure Sense</CardTitle>
+                  <CardDescription>Check Eligibility Criteria of Proposals</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <Image src={cardGame} alt="project-1" height={150}/>
+                </CardContent>
+              </a>
+            </Card>
+            <Card className='hover:bg-hoverWhite'>
+              <a href="https://66e575bf49004939ba38a25f--rainbow-sunflower-71542f.netlify.app/" target="_blank" rel="noopener noreferrer">
+                <CardHeader>
+                  <CardTitle>Procure Sense</CardTitle>
+                  <CardDescription>Check Eligibility Criteria of Proposals</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <Image src={voteApp} alt="project-1" height={150}/>
+                </CardContent>
+              </a>
+            </Card>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
